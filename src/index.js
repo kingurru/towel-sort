ae76996ab1deb941f71e52b8a715e05c7ce2ebba
kingurru <kingnikoriginal@gmail.com>
2021-03-08 20:46:56 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix = []) {
+    let arr = []
+    let arrTowel = []
+    for (let i = 0; i < matrix.length; i++) {
+        i % 2 ? arr.push(matrix[i].reverse()) : arr.push(matrix[i])
+    }
+    arr.map(item => item.map(subItem => arrTowel.push(subItem)))
+    console.log(arrTowel)
+    return arrTowel
 }
